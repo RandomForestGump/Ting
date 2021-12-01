92fe2ae614357df6461e14b1e43879e6060145bb
paleknai <paleknai@buffalo.edu>
2021-11-30 19:39:38 -0500

    Search

--- ting/semantron/views.py
+++ ting/semantron/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from core_module.search import getTweet
 
 # Create your views here.
 
+def search(request):
+
+    try:
+        load_query = request.body.decode('utf-8')
+        query = json.loads(load_query)
+
+        status_code, eta_response = getTweet(query)
+
+        return JsonResponse({'status': status_code, 'body': eta_response, 'API_VERSION': 2.0})
+
+    except Exception as e:
+#        logger.error(traceback.format_exc())
+        response = {"message": "error {}".format(str(e))}
+        return JsonResponse({'status': 500, 'body': response, 'API_VERSION': 2.0})
