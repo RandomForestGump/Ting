c0c82389e54da02eaff16f7c64560613b462682e
Rajat Jain <rajatjain@Rajats-MacBook-Pro.local>
2021-12-10 22:23:55 -0500

    Analyzing search issue

--- ting/core_module/search/search.py
+++ ting/core_module/search/search.py
@@ -7,6 +7,7 @@ def getTweet(query):
     ip = '18.118.247.209'
     query = query.replace(":", "\:")
     query = urllib.parse.quote(query, safe='')
+    print(query)
     # inurl =f'http://3.144.198.12:8983/solr/{CORE_NAME}/select?bq=text_en%3A({query})%5E2%20text_ru%3A({query})%5E2%20text_de%3A({query})%5E2&defType=dismax&fl=id%2Cscore&indent=true&q.op=OR&q={query}&qf=text_de%20text_en%20text_ru&rows=20'
     inurl = f'http://{ip}:8983/solr/IRF21P3/select?defType=edismax&q.op=OR&q={query}&qf=tweet_text&rows=1000'
     print(inurl)
