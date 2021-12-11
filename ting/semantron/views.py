6cf4881a1779e0136452f61b6a1e6b62b1bff817
Rajat Jain <rajatjain@Rajats-MacBook-Pro.local>
2021-12-10 22:35:44 -0500

    Adding Error Handler

--- ting/semantron/views.py
+++ ting/semantron/views.py
@@ -19,33 +19,34 @@ def search(request):
         query = request.GET['query_term']
         docs = getTweet(query)
 
-        analysis = DynamicAnalyzer(docs)
+        if len(docs) == 0:
+            result['documents'] = None
+            return JsonResponse({'status': 200, 'body': result})
 
-        tweet_type = analysis.get_tweet_types()
+        else:
+            analysis = DynamicAnalyzer(docs)
+            tweet_type = analysis.get_tweet_types()
+            tweet_sentiment = analysis.get_sentiment()
+            poi_distribution = analysis.get_poi_distribution()
+            keyword_wc = analysis.get_hashtag_wc()
+            extreme = analysis.get_xtreme_tweets()
+            antivax = analysis.anti_vaxxer()
 
-        tweet_sentiment = analysis.get_sentiment()
+            result['documents'] = docs
+            print('Docs Fetched')
+            result['tweet_type'] = tweet_type
+            result['poi_dist'] = poi_distribution
+            print('Tweet types and poi distribution Fetched')
+            result['keywords'] = keyword_wc
+            print('Keywords Fetched')
+            result['antivaccine_tweets'] = antivax
+            print('Antivaccine Fetched')
+            result['extreme'] = extreme
+            print('Extreme Fetched')
+            result['tweet_sentiment'] = tweet_sentiment
+            print('Main sentiment Fetched')
 
-        poi_distribution = analysis.get_poi_distribution()
-
-        keyword_wc = analysis.get_hashtag_wc()
-        extreme = analysis.get_xtreme_tweets()
-        antivax = analysis.anti_vaxxer()
-
-        result['documents'] = docs
-        print('Docs Fetched')
-        result['tweet_type'] = tweet_type
-        result['poi_dist'] = poi_distribution
-        print('Tweet types and poi distribution Fetched')
-        result['keywords'] = keyword_wc
-        print('Keywords Fetched')
-        result['antivaccine_tweets'] = antivax
-        print('Antivacc Fetched')
-        result['extreme'] = extreme
-        print('Extreme Fetched')
-        result['tweet_sentiment'] = tweet_sentiment
-        print('Main sentiment Fetched')
-
-        return JsonResponse({'status': 200, 'body': result})
+            return JsonResponse({'status': 200, 'body': result})
 
     except Exception as e:
 
