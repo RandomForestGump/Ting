793265607d318b4524cb8585d05f015a96ba6611
grvzzz <gauravnatekar@gauravs-mbp.myfiosgateway.com>
2021-12-11 01:40:40 -0500

    front end

---
+++ build/precache-manifest.50ea91b64f7a8ef7d58bcead753111a9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97729b80cdae1a7e63f5da76d0022855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c816229a93d07c4438",
+    "url": "/static/css/2.4a3f91d7.chunk.css"
+  },
+  {
+    "revision": "26c816229a93d07c4438",
+    "url": "/static/js/2.8b05d64c.chunk.js"
+  },
+  {
+    "revision": "bc6e22e95b0e96d7cc199034be461533",
+    "url": "/static/js/2.8b05d64c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2602e824e9b7d3ce68f",
+    "url": "/static/js/main.c84b8020.chunk.js"
+  },
+  {
+    "revision": "8aba14c5515e5362888a",
+    "url": "/static/js/runtime-main.66cf0057.js"
+  },
+  {
+    "revision": "5d32d2bb50dd0498a0dad4d8be7e827a",
+    "url": "/static/media/dxicons.5d32d2bb.ttf"
+  },
+  {
+    "revision": "6ea5ec503da2a9162e9a85757a471176",
+    "url": "/static/media/dxicons.6ea5ec50.woff2"
+  },
+  {
+    "revision": "d1b921dd657d23fa910b1e52a976b914",
+    "url": "/static/media/dxicons.d1b921dd.woff"
+  }
+]);
